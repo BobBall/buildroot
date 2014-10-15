07c8415c171b9e954848654788f08575b8cf6ca8
Bob Ball <bob.ball@citrix.com>
2014-10-15 11:41:37 +0100

    'make install' should only build the packages needed to install
    Needed to add a -deps target to support this.
    To ensure that the install script doesn't diverge in what it is installing, move
    the definition of what it will install to the deps file along-side what it is making

--- specdep.py
+++ specdep.py
@@ -98,6 +98,14 @@ def buildrequires_for_rpm(spec, provides_to_rpm):
             print "%s: %s" % (rpmpath, buildreqrpm)
 
 
+def requires_for_rpm(spec, provides_to_rpm):
+    for buildreq in spec.requires():
+        # Some requires come from the system repository
+        if provides_to_rpm.has_key(buildreq):
+            buildreqrpm = provides_to_rpm[buildreq]
+            print "%s-deps: %s" % (spec.name(), buildreqrpm)
+
+
 def parse_cmdline():
     """
     Parse command line options
@@ -151,6 +159,7 @@ def main():
         download_rpm_sources(spec)
         build_rpm_from_srpm(spec)
         buildrequires_for_rpm(spec, provides_to_rpm)
+        requires_for_rpm(spec, provides_to_rpm)
         print ""
 
     # Generate targets to build all srpms and all rpms
@@ -167,8 +176,8 @@ def main():
     print ""
     print "srpms: " + " \\\n\t".join(all_srpms)
     print ""
-    print "install: all"
-    print "\t. scripts/%s/install.sh" % build_type()
+    print "install: xenserver-core xenserver-core-deps"
+    print "\t. scripts/%s/install.sh xenserver-core" % build_type()
 
 
 if __name__ == "__main__":
